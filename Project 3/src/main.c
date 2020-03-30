6e2427dcc9efc8910ae88d6845f8e7b3b11bcc19
wang8635 <wang8635@umn.edu>
2020-03-29 22:49:46 -0500

    add main
    add main

--- Project 3/src/main.c
+++ Project 3/src/main.c
@@ -1,11 +1,18 @@
 //main.c program to translate the virtual addresses from the virtual.txt
 //into the physical frame address. Project 3 - CSci 4061
-
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <limits.h>
 #include "vmemory.h"
 
 #define INPUT_FILE "../bin/virtual.txt"
+#define MSB_20BITS 0xfffff000
 
 int main(int argc, char* argv[])
 {
@@ -18,8 +25,32 @@ int main(int argc, char* argv[])
 	initialize_vmanager(policy);
 
 	//TODO: Fill the algorithm by reading from INPUT_FILE
-	
-	//Free the page table
+
+
+	FILE * fd = fopen(INPUT_FILE, "r");
+	if (fd == NULL)
+	{
+		printf("Read file error\n");
+		return 0;
+	}
+	char* cont = NULL;
+	size_t length = 0;
+	ssize_t read;
+	while((read = getline(&cont,&length,fd)) != -1)
+	{
+		char* str;
+		unsigned int vadd = strtoul(cont, NULL, 16);
+		unsigned int vadd_pn = (vadd & 0xfffff000) >> 12;
+		int offset = vadd & 0xffff00000;
+
+		print_tlb();
+		print_physical_address(1, offset);
+		if(get_tlb_entry(vadd_pn) == -1) {
+			populate_tlb(vadd_pn, translate_virtual_address(vadd));
+		}
+	}
+	fclose(fd);
+	// Free the page table
 	free_resources();
 	return 0;
 }
