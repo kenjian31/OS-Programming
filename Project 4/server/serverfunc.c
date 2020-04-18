4f840f8ce2ee2b6647524451be817c5ea2aa53c6
Fei Gao <feigao@Feis-MacBook-Pro.local>
2020-04-18 17:15:09 -0500

    inside main version

--- Project 4/server/serverfunc.c
+++ Project 4/server/serverfunc.c
@@ -4,34 +4,38 @@
  #include <string.h>
 
 // Global array
-int char_count[26]; 
-for( x= 0; x < 26; x++)
-{
-    char_count[x] = 0;
-}
+int char_count[26] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
-
-void count_file(char* path){
-    int char_count[26]; 
-    for( x= 0; x < 26; x++){
-     char_count[x] = 0;
-     }
-    char c;
-    while((c = getchar()) != EOF){
-       if(isalpha(c)){
-           c = tolower(c);
-            char_count[c-'a']++;
-        }
-     }
-    //  for(i = 0; i<26; i++){
-    //      addcount(i, char_count[i]);
-    //  }
+ void addcount(int alphai, int add){
+     char_count[alphai] += add;
  }
 
- main() // main for test purpose. 
+ int main() // main for test purpose. 
  {
+    char *path = "Makefile";
 
 
- 
-  
+    FILE* fp = fopen(path,"r");
+    char c;
+    // Local array to store char count
+    int char_count_local[26]; 
+    for( int i = 0; i < 26; i++){
+     char_count_local[i] = 0;
+     }
+    while ((c = fgetc(fp)) != EOF) {
+		if(isalpha(c)){
+            c = tolower(c);
+            char_count_local[c-'a']++;
+		}
+     }
+    fclose(fp);
+    for(int i = 0; i<26; i++){
+         addcount(i, char_count_local[i]);
+         }
+
+
+
+    for(int i1 = 0; i1 < 26; i1++){
+       printf("%d# ", char_count[i1]);
+       }
  }
